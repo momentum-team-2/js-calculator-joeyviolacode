c3909dd0ead204ad07ce704a7d645d774beb77ea
joeyviolacode <joeyviolacode@gmail.com>
2020-05-28 23:19:27 -0400

    Finished commit with TODO in .js file.

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
+/*
+Finished and TODOs:
+Catches invalid input and throws an alert, allowing user to backspace and enter new input
+Basic keyboard entry implemented (and commented out, because) logic for excluding letters and accepting Shift-reliant values not implemented
+Rudimentary colors and button press CSS included, though not the focus of assignment
+Need to implement spaces around operators for cleaner look in display...add operator logic
+
+*/
+
 let inputLocation = document.querySelector(".display-container")
 let text = ""
 
+//Set EventListeners for all buttons
 let inputButtons = document.querySelectorAll(".input-button")
 for (let button of inputButtons) {
     button.addEventListener("click", updateInput)
@@ -9,7 +19,8 @@ document.querySelector("#equals").addEventListener("click", evaluateInput)
 document.querySelector("#C").addEventListener("click", resetInput)
 document.querySelector("#BS").addEventListener("click", backspace)
 
-
+//Test Case for keypresses...so below basic functions for logic...works, commented out
+//document.querySelector(".everything").addEventListener("keydown", handleKeydown)
 
 //Basic functions for updating the input in various ways
 function updateInput(event) {
@@ -27,10 +38,14 @@ function resetInput() {
 }
 
 function evaluateInput() {
-    let value = eval(text)
-    resetInput()
-    text = value;
-    setInnerHTML(value)
+    try {
+        let value = eval(text)
+        resetInput()
+        text = value
+        setInnerHTML(value) 
+    } catch(e) {
+        alert("That's not valid.  Try again.")
+    }
 }
 
 function backspace() {
@@ -44,3 +59,22 @@ function setInnerHTML(value) {
     inputLocation.innerHTML = `<p class="display">${value}</p>`
 }
 
+
+//test case for keyboard input...
+//works...would have to add logic for excluding non-numbers and including
+//characters produced with Shift key...certain I can do it, so saving some
+//life by not doing it at the moment
+//Currently works for any single-input keys, not attached to enter to evaluate
+//Commenting out, as it currently accepts all the things.
+/*function updateKeydown(code) {
+    if (String.fromCharCode(code) === "." && text.length === 0) {
+        text += "0."
+    } else {
+        text += String.fromCharCode(code)
+    }
+    setInnerHTML(text)
+}
+
+function handleKeydown(event) {
+    updateKeydown(event.keyCode)
+}*/
